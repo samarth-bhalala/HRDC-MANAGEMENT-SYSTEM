5df791218d319fb851c6e970695b9194f5dc4eb5
samarth-bhalala <sambhubhalala@gmail.com>
2025-08-20 22:07:07 +0530

    Revert "commented error code"
    This reverts commit 3a45fe405a0c7511aa8c267273f34155b17ccf77.

--- HRDC/HRDC/Controllers/Admin/EmployeeController.cs
+++ HRDC/HRDC/Controllers/Admin/EmployeeController.cs
@@ -1,51 +1,51 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-//namespace HRDC.Controllers.Admin
-//{
-//    public class EmployeeController : Controller
-//    {
-//        [HttpGet]
-//        public IActionResult EditEmployee(int id)
-//        {
-//            var emp = employees.FirstOrDefault(e => e.EmployeeId == id);
-//            if (emp == default)
-//                return NotFound();
+namespace HRDC.Controllers.Admin
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public IActionResult EditEmployee(int id)
+        {
+            var emp = employees.FirstOrDefault(e => e.EmployeeId == id);
+            if (emp == default)
+                return NotFound();
 
-//            var model = new EmployeeEditViewModel
-//            {
-//                EmployeeId = emp.EmployeeId,
-//                Name = emp.Name,
-//                Email = emp.Email,
-//                DepartmentId = emp.DepartmentId,
-//                DesignationId = emp.DesignationId,
-//                EmployeeTypeId = emp.EmployeeTypeId,
-//                Departments = departments.Select(d => new SelectListItem(d.Name, d.Id.ToString())).ToList(),
-//                Designations = designations.Select(d => new SelectListItem(d.Name, d.Id.ToString())).ToList(),
-//                EmployeeTypes = employeeTypes.Select(e => new SelectListItem(e.Name, e.Id.ToString())).ToList()
-//            };
+            var model = new EmployeeEditViewModel
+            {
+                EmployeeId = emp.EmployeeId,
+                Name = emp.Name,
+                Email = emp.Email,
+                DepartmentId = emp.DepartmentId,
+                DesignationId = emp.DesignationId,
+                EmployeeTypeId = emp.EmployeeTypeId,
+                Departments = departments.Select(d => new SelectListItem(d.Name, d.Id.ToString())).ToList(),
+                Designations = designations.Select(d => new SelectListItem(d.Name, d.Id.ToString())).ToList(),
+                EmployeeTypes = employeeTypes.Select(e => new SelectListItem(e.Name, e.Id.ToString())).ToList()
+            };
 
-//            return View(model);
-//        }
+            return View(model);
+        }
 
-//        [HttpPost]
-//        public IActionResult EditEmployee(EmployeeEditViewModel model)
-//        {
-//            if (!ModelState.IsValid)
-//            {
-//                // Reload dropdowns if model invalid
-//                model.Departments = departments.Select(d => new SelectListItem(d.Name, d.Id.ToString())).ToList();
-//                model.Designations = designations.Select(d => new SelectListItem(d.Name, d.Id.ToString())).ToList();
-//                model.EmployeeTypes = employeeTypes.Select(e => new SelectListItem(e.Name, e.Id.ToString())).ToList();
+        [HttpPost]
+        public IActionResult EditEmployee(EmployeeEditViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Reload dropdowns if model invalid
+                model.Departments = departments.Select(d => new SelectListItem(d.Name, d.Id.ToString())).ToList();
+                model.Designations = designations.Select(d => new SelectListItem(d.Name, d.Id.ToString())).ToList();
+                model.EmployeeTypes = employeeTypes.Select(e => new SelectListItem(e.Name, e.Id.ToString())).ToList();
 
-//                return View(model);
-//            }
+                return View(model);
+            }
 
-//            // TODO: Save update to database here.
-//            // For example: update employee record with model values.
+            // TODO: Save update to database here.
+            // For example: update employee record with model values.
 
-//            TempData["Message"] = "Employee updated successfully!";
-//            return RedirectToAction("ViewEmployee"); // Redirect back to list
-//        }
-//    }
-//}
+            TempData["Message"] = "Employee updated successfully!";
+            return RedirectToAction("ViewEmployee"); // Redirect back to list
+        }
+    }
+}
