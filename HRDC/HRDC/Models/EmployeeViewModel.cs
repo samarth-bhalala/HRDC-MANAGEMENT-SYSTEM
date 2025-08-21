a68daf7a0969fb35a9c12fda3f2ccff1a9390d38
Manan Patel <manan15062005@gmail.com>
2025-08-21 12:47:42 +0530

    Changes Commited

--- HRDC/HRDC/Models/EmployeeViewModel.cs
+++ HRDC/HRDC/Models/EmployeeViewModel.cs
@@ -1,4 +1,7 @@
-﻿public class EmployeeViewModel
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+public class EmployeeViewModel
 {
     public int EmployeeId { get; set; }
     public string Name { get; set; }
@@ -6,4 +9,11 @@
     public string Department { get; set; }
     public string Designation { get; set; }
     public string EmployeeType { get; set; }
+}
+    
+public class EmployeeListViewModel
+{
+    public List<EmployeeViewModel> Employees { get; set; } = new List<EmployeeViewModel>();
+    public string SelectedType { get; set; }
+    public List<SelectListItem> EmployeeTypes { get; set; } = new List<SelectListItem>();
 }
